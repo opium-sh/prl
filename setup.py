3e21f8c7c87cfc7aee84d9e264c3a8b2bc549076
ptempczyk <37421284+ptempczyk@users.noreply.github.com>
2020-02-12 10:42:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name="prl",
-    version="0.1",
-    description="Package for reinforcement learning research.",
+    version="0.1.0",
+    description="Framework for reinforcement learning research.",
     author="Piotr Tempczyk & opium.sh",
     author_email="piotr.tempczyk@opium.sh",
-    url="https://github.com/opium-pl/prl",
+    url="https://github.com/opium-sh/prl",
 )
